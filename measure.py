7608204b05a902fcf6c6efa585906d7195312acb
Dang Tu <tu07122001@gmail.com>
2023-04-11 02:22:32 +0700

    affine_gap and smith_waterman

--- measure.py
+++ measure.py
@@ -1,30 +1,20 @@
-import numpy as np
 
-def edit_distance():
+def affine_gap(c_0, c_r, c_xy):
     def inner(string_1, string_2):
-        n = len(string_1)
-        m = len(string_2)
-        d = np.zeros((n, m))
-        for i in range(n):
-            d[i, 0] = i
-        for j in range(m):
-            d[0, j] = j
-            
-        for i in range(1, n):
-            for j in range(1, m):
-                copy_subtitute = 0
-                if string_1[i] == string_2[j]:
-                    copy_subtitute = d[i - 1, j - 1] + c_xy(string_1[i], string_2[j])
-                else:
-                    copy_subtitute = d[i - 1, j - 1] + 1
-
-                delete_1 = d[i - 1, j] + 1
-                insert_2 = d[i, j - 1] + 1
-
-                d[i, j] = min(copy_subtitute, delete_1, insert_2)
-
-        result = 1 - d[n -1, m -1]/ max(n, m)
-        return result
+        m, n = len(string_1), len(string_2)
+        s = [[0] * (n + 1) for _ in range(m + 1)]
+        M = [[0] * (n + 1) for _ in range(m + 1)]
+        I_y = [[0] * (n + 1) for _ in range(m + 1)]
+        I_x = [[0] * (n + 1) for _ in range(m + 1)]
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                M[i][j] = max(M[i - 1][j - 1] + c_xy(string_1[i - 1],string_2[j - 1]) , 
+                                I_x[i-1][j-1] + c_xy(string_1[i - 1],string_2[j - 1]),
+                                I_y[i-1][j-1] + c_xy(string_1[i - 1],string_2[j - 1]))
+                I_x[i][j] = max(I_x[i - 1][j] - c_r, M[i - 1][j] - c_0 )
+                I_y[i][j] = max(I_y[i][j - 1] - c_r, M[i][j - 1] - c_0 )
+                s[i][j] = max(M[i][j], I_y[i][j], I_x[i][j])
+        return s[m][n]
     
     return inner
 
@@ -32,3 +22,25 @@ def edit_distance():
 affine_gap_func = affine_gap(1, 0.5, lambda char_1, char2: 2 if char_1 == char2 else -1)
 
 affine_gap_func("Tuan Trinh", "Tuan NB Trinh")
+
+
+def smith_waterman(c_g, c_xy):
+    def inner(string_1, string_2):
+        m, n = len(string_1), len(string_2)
+        s = [[0] * (n + 1) for _ in range(m + 1)]
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                score = max(
+                    s[i-1][j-1] + c_xy(string_1[i-1], string_2[j-1]),
+                    s[i-1][j] - c_g,
+                    s[i][j-1] - c_g,
+                    0
+                )
+                s[i][j] = score
+        return s[m][n]
+    return inner
+
+smith_waterman_func = smith_waterman(2, lambda char_1, char2: 2 if char_1 == char2 else -1)
+
+print(smith_waterman_func("Prof. John R. Smith, Univ of Wisconsin", "John R.Smith, Professor"))
+    
