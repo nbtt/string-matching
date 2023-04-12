3a470e2ef2fc3c73e99926e5ea8691b086bd6434
tuantnb <tuantnb@vng.com.vn>
2023-04-12 07:08:03 +0700

    add full flow

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 import load_data
 import transform
+import measure
 import pandas as pd
+from functools import reduce
 from pprint import pprint
 
 def make_ground_truth_name(profile_1_name, profile_2_name):
@@ -62,22 +64,98 @@ def transform_func(concat_columns):
 
 ## Do transformation
 strings: dict[str, pd.Series] = {}
-dblp_acm_colunms = ['title', 'authors', 'venue', 'year']
+dblp_acm_colunms = ['title']
 dblp_acm_transform = transform_func(dblp_acm_colunms)
 for name in [N_DBLP, N_ACM]:
     strings[name] = data[name].apply(dblp_acm_transform, axis=1)
 pprint(strings[N_DBLP][:10].to_list())
-
-# Selection function
-def base_select_func(profile_2_name):
-    '''select profile 2 list that might be match with profile 1'''
-    def inner(profile_1_string):
-        return strings[profile_2_name]
-    
-select = base_select_func(N_ACM)
+print(type(strings[N_DBLP][0]))
 
 # Mesaure simmilarity function
+affine_gap_func = measure.affine_gap(1, 0.5, lambda char_1, char2: 2 if char_1 == char2 else -1)
+smith_waterman_func = measure.smith_waterman(2, lambda char_1, char2: 2 if char_1 == char2 else -1)
+
+def create_measure_func(string_1: str):
+    def inner(data_item: str):
+        return pd.Series({
+            'affine_gap': affine_gap_func(string_1, data_item),
+            'smith_waterman': smith_waterman_func(string_1, data_item)
+        })
+    return inner
+
+result = {
+    'affine_gap': [],
+    'smith_waterman': []
+}
+measures_result = {
+    'affine_gap': [],
+    'smith_waterman': []
+}
+
+def create_apply_measure_func(profile_2_name, threshold):
+    def inner(data_item):
+        profile_1_index = data_item[1]
+        profile_1_string = data_item[0]
+        
+        measure_func = create_measure_func(profile_1_string)
+        measures = strings[profile_2_name][1340:1350].apply(measure_func)
+        measures_matrix = measures.applymap(lambda measure: measure >= threshold)
+        measures_index = pd.DataFrame()
+        measures_index = {
+            measure_name: measures_matrix.index[measures_matrix[measure_name]] for measure_name in measures_matrix
+        }
+        # measure_chosen = {
+        #     'affine_gap': measures_matrix.index[measures_matrix['affine_gap']],
+        #     'smith_waterman': measures_matrix.index[measures_matrix['smith_waterman']],
+        # }
+
+        measures_chosen = {
+            measure_name: list(measures_index[measure_name].map(lambda index: [profile_1_index, index])) for measure_name in measures_index
+        }
+
+        # measures_chosen = list(measures_index['affine_gap'].map(lambda index: [profile_1_index, index]))
+        # if measures_chosen is not []:
+        #     result['affine_gap'].append(measures_chosen)
+
+        for measure_name in measures_chosen:
+            if len(measures_chosen[measure_name]) > 0:
+                result[measure_name].append(measures_chosen[measure_name])
+
+            measures_result[measure_name].append({
+                profile_1_index: measures
+            })
+        
+        return measures
+    return inner
 
 # Do string matching
+threshold = 0.8
+apply_measure_func = create_apply_measure_func(N_ACM, threshold)
+strings_frame = strings[N_DBLP][1820:1830].to_frame(name='string')
+strings_frame['index'] = strings_frame.index
+strings_frame.apply(apply_measure_func, axis=1)
+
+# Unpack result
+result = {
+    measure_name: reduce(lambda x, y: x + y, result[measure_name]) for measure_name in result
+}
+
+pprint(measures_result)
+pprint(result)
 
 # Compare with ground truth
+def compute_accuracy(result, ground_truth_name):
+    profile_names = ground_truth_name.split('_')[:-1]
+    ground_truth = data[ground_truth_name]
+    def match_indexes(indexes):
+        return ((ground_truth[profile_names[0]] == indexes[0]) & (ground_truth[profile_names[1]] == indexes[1])).any()
+    result_compare = {
+        measure_name: list(map(match_indexes, result[measure_name])) for measure_name in result
+    }
+    return {
+        measure_name: len(result_compare[measure_name]) / len(ground_truth.index) for measure_name in result_compare
+    }, result_compare
+
+# Acuracy
+accuracy, _ = compute_accuracy(result, N_DBLP_ACM)
+print(accuracy)
