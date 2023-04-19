a6cff2d7a23c1ac77bd776c51b9b6ab9c4f0daf2
tuantnb <tuantnb@vng.com.vn>
2023-04-19 19:49:23 +0700

    fix precision recall f1

--- metric.py
+++ metric.py
@@ -16,18 +16,22 @@ def compute_accuracy(data, prediction, ground_truth_name):
         measure_name: prediction_compare[measure_name] / len(ground_truth.index) for measure_name in prediction_compare
     }, prediction_compare
 
-def compute_precision_recall_f1(prediction_count: dict[str, int], prediction_compare: dict[str, int], ground_truth_name):
-    def compute_metric(prediction_compare_item):
-        measure_name = prediction_compare_item[0]
-        true_positive = prediction_compare_item[1]
-
-        false_positive = 0
+def compute_precision_recall_f1(data, prediction_count: dict[str, int], prediction_compare: dict[str, int], ground_truth_name):
+    def compute_metric(measure_name, true_positive, count_positive, count_true):
+        precision = true_positive / count_positive
+        recall = true_positive / count_true
+        f1_score = 2 * precision * recall / (precision + recall)
 
         return pd.Series({'precision': precision, 'recall': recall, 'f1_score': f1_score}, name=measure_name)
     
-    profile_names = ground_truth_name.split('_')[:-1]
     ground_truth = data[ground_truth_name]
-    
-    return {
-        item[0]: map(compute_metric, item) for item in prediction_compare.items()
-    }
+    count_true = len(ground_truth)
+
+    result = {}
+
+    for measure_name, true_positive in prediction_compare.items():
+        count_positive = prediction_count[measure_name] # number of predicted values
+        
+        result[measure_name] = compute_metric(measure_name, true_positive, count_positive, count_true)
+
+    return result
